2a929a0196b15fbf191ae67dfa31895f07250fc7
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-06 22:12:53 -0300

    style: more security when returning an existing guest

--- src/repositories/GuestRepository.ts
+++ src/repositories/GuestRepository.ts
@@ -11,7 +11,7 @@ export class GuestRepository {
 
     async findByEmail(email: string){
         const guest = await this.database.findOne({email})
-        return guest
+        return !!guest
     }
 
     async create(data: IGuestDTO): Promise<IGuest> {
