350386f6245e800398d429d48a0d3ae9fac54994
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-09 23:57:40 -0300

    feate: schema adm

---
+++ src/schemes/AdminSchema.ts
@@ -0,0 +1,13 @@
+import * as yup from 'yup'
+import { IAdm } from '../entities/IAdm'
+import { TypedRequest } from '../utils/typedRequest'
+
+export const admSchemaValidateLogger = yup.object<IAdm>({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+})
+
+export namespace AdmLogger {
+    export type BodyType = TypedRequest<typeof admSchemaValidateLogger>;
+    export const schema = yup.object().shape({ body: admSchemaValidateLogger})
+}
