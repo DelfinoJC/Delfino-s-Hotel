6274c728a67c0043d783a66151ccaf3e931b77f5
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-10 21:25:54 -0300

    style: formatting in the repositories files

--- src/controllers/AdmController.ts
+++ src/controllers/AdmController.ts
@@ -1,15 +1,17 @@
-import { AdminModel } from '../models/AdminModel'
-import { AdmService } from '../services/AdmService'
-import { AdminRepository } from '../repositories/AdmRepository'
-import { Request, Response } from 'express'
+import { AdminModel } from "../models/AdminModel";
+import { AdmService } from "../services/AdmService";
+import { StatusCode } from "../utils/statusCode";
+import { AdminRepository } from "../repositories/AdmRepository";
+import { Request, Response } from "express";
 
-const repository = new AdminRepository(AdminModel)
-const service = new AdmService(repository)
+const repository = new AdminRepository(AdminModel);
+const service = new AdmService(repository);
 
-export async function loggerAdmController(req: Request, res: Response){
-    try{
-        const login = await service.logger(req.body)
-        return login
-    }
-    catch(error){}
+export async function loggerAdmController(req: Request, res: Response) {
+  try {
+    const login = await service.logger(req.body);
+    res.status(StatusCode.OK).json({ token: login });
+  } catch (error) {
+    res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
+  }
 }
--- src/controllers/GuestController.ts
+++ src/controllers/GuestController.ts
@@ -1,37 +1,35 @@
-import { Request, Response} from 'express'
-import { GuestService } from '../services/GuestService'
-import { GuestRepository } from '../repositories/GuestRepository'
-import { GuestModel } from '../models/GuestModel'
-import { StatusCode } from '../utils/statusCode'
+import { GuestModel } from "../models/GuestModel";
+import { StatusCode } from "../utils/statusCode";
+import { GuestService } from "../services/GuestService";
+import { Request, Response } from "express";
+import { GuestRepository } from "../repositories/GuestRepository";
 
-const repository = new GuestRepository(GuestModel)
-const service = new GuestService(repository)
+const repository = new GuestRepository(GuestModel);
+const service = new GuestService(repository);
 
 export async function createGuest(req: Request, res: Response) {
-    try{
-        const newGuest = await service.createGuest(req.body)
-        res.status(StatusCode.CREATE).json({ newGuest })
-    }
-    catch(error){
-        res.status(StatusCode.BAD_REQUEST).json({ message: error.message })
-    }
+  try {
+    const newGuest = await service.createGuest(req.body);
+    res.status(StatusCode.CREATE).json({ newGuest });
+  } catch (error) {
+    res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
+  }
 }
 
 export async function loggerGuest(req: Request, res: Response) {
-    try{
-        const token = await service.logger(req.body)
-        res.status(StatusCode.CREATE).json( { token } )
-    }
-    catch(error){
-        res.status(StatusCode.BAD_REQUEST).json( { message: error.message } )
-    }
+  try {
+    const token = await service.logger(req.body);
+    res.status(StatusCode.CREATE).json({ token });
+  } catch (error) {
+    res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
+  }
 }
 
-export async function findAllGuest(req: Request, res: Response){
-    try {
-        const guests = await service.findAllGuest()
-        res.status(StatusCode.OK).json({ guests })
-    } catch (error) {
-        res.status(StatusCode.BAD_REQUEST).json({ messed: error.messed })
-    }
+export async function findAllGuest(req: Request, res: Response) {
+  try {
+    const guests = await service.findAllGuest();
+    res.status(StatusCode.OK).json({ guests });
+  } catch (error) {
+    res.status(StatusCode.BAD_REQUEST).json({ messed: error.messed });
+  }
 }
