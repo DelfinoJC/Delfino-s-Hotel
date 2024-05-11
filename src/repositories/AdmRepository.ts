a3dcaef47cf4dc1406862f7ee1ef8d30e015262b
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-10 21:35:41 -0300

    style: formatting and improving template files

--- src/repositories/AdmRepository.ts
+++ src/repositories/AdmRepository.ts
@@ -1,15 +1,15 @@
-import { Model } from 'mongoose'
-import { IAdm } from '../entities/IAdm'
+import { IAdm } from "../entities/IAdm";
+import { Model } from "mongoose";
 
 export class AdminRepository {
-    private database: Model<IAdm>
+  private database: Model<IAdm>;
 
-    constructor(db: Model<IAdm>){
-        this.database = db
-    }
+  constructor(db: Model<IAdm>) {
+    this.database = db;
+  }
 
-    async findByEmail (email: string): Promise<IAdm | null> {
-        const adm = await this.database.findOne({email})
-        return adm
-    }
+  async findByEmail(email: string): Promise<IAdm | null> {
+    const adm = await this.database.findOne({ email });
+    return adm;
+  }
 }
--- src/repositories/GuestRepository.ts
+++ src/repositories/GuestRepository.ts
@@ -1,27 +1,26 @@
-import { mongoose } from '../database/connectionDatabase'
-import { Model } from 'mongoose'
-import { IGuest, IGuestDTO, IGuestLogin } from '../entities/IGuest'
+import { mongoose } from "../database/connectionDatabase";
+import { Model } from "mongoose";
+import { IGuest, IGuestDTO, IGuestLogin } from "../entities/IGuest";
 
 export class GuestRepository {
-    private database: Model<IGuest>
+  private database: Model<IGuest>;
 
-    constructor(db: Model<IGuest>){
-        this.database = db
-    }
+  constructor(db: Model<IGuest>) {
+    this.database = db;
+  }
 
-    async findByEmail(email: string): Promise<IGuest | null>{
-        const guest = await this.database.findOne({email})
-        return guest
-    }
-    
-    async findAllGuest(){
-        const allUsers = await this.database.find()
-        return allUsers
-    }
+  async findByEmail(email: string): Promise<IGuest | null> {
+    const guest = await this.database.findOne({ email });
+    return guest;
+  }
 
-    async create(data: IGuestDTO): Promise<IGuest> {
-        const save = await this.database.create(data)
-        return save
-    }
-    
+  async findAllGuest() {
+    const allUsers = await this.database.find();
+    return allUsers;
+  }
+
+  async create(data: IGuestDTO): Promise<IGuest> {
+    const save = await this.database.create(data);
+    return save;
+  }
 }
