d188e7ff4162c2e2d77fd89ad65f878b0d401cfb
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-10 21:43:55 -0300

    style: formatting and improving template files services

--- src/services/GuestService.ts
+++ src/services/GuestService.ts
@@ -1,11 +1,16 @@
-import { IGuest, IGuestDTO, IGuestLogin, IGuestTOKEN } from "../entities/IGuest";
+import {
+  IGuest,
+  IGuestDTO,
+  IGuestLogin,
+  IGuestTOKEN,
+} from "../entities/IGuest";
 import { Crypto } from "../provider/cryptograph";
-import { authGuest } from '../configs/authGuestConfig'
-import { Token } from '../provider/createToken'
+import { authGuest } from "../configs/authGuestConfig";
+import { Token } from "../provider/createToken";
 import { GuestRepository } from "../repositories/GuestRepository";
 
 const crypto = new Crypto();
-const token = new Token()
+const token = new Token();
 
 export class GuestService {
   private repository: GuestRepository;
@@ -15,7 +20,6 @@ export class GuestService {
   }
 
   async createGuest(data: IGuestDTO) {
-
     const guestExistWithEmail = await this.repository.findByEmail(data.email);
     if (guestExistWithEmail) {
       throw new Error(`This email has already been registered`);
@@ -31,22 +35,29 @@ export class GuestService {
     return await this.repository.create(newGuest);
   }
 
-  async logger(data: IGuestLogin): Promise<IGuestTOKEN>{
-    const { email, password } = data
+  async logger(data: IGuestLogin): Promise<IGuestTOKEN> {
+    const { email, password } = data;
 
-    const guest = await this.repository.findByEmail( email )
-    if(!guest){
-      throw new Error(`Incorrect password or email`)
+    const guest = await this.repository.findByEmail(email);
+    if (!guest) {
+      throw new Error(`Incorrect password or email`);
     }
 
-    const comparesWhetherPasswordsAreTheSame = await crypto.comperePassword(password, guest.password)
+    const comparesWhetherPasswordsAreTheSame = await crypto.comperePassword(
+      password,
+      guest.password
+    );
     if (!comparesWhetherPasswordsAreTheSame) {
-      throw new Error(`Incorrect password or email`)
+      throw new Error(`Incorrect password or email`);
     }
 
-    const tokenCreated = token.tokenJWT(guest._id.toString(), false, authGuest.secret)
+    const tokenCreated = token.tokenJWT(
+      guest._id.toString(),
+      false,
+      authGuest.secret
+    );
 
-    return { token: tokenCreated }
+    return { token: tokenCreated };
   }
 
   async findAllGuest() {
