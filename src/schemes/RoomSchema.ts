0667db3a205cc709209fca2036f2fc9f990f1147
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-05-16 20:23:07 -0300

    chore: add new validate schema to update room status

--- src/schemes/RoomSchema.ts
+++ src/schemes/RoomSchema.ts
@@ -11,10 +11,26 @@ export const roomSchemaValidate = yup.object<IRoom>({
   status: yup
     .string()
     .oneOf(["available", "busy", "under maintenance"])
-    .required(),
 });
 
 export namespace CreateRoom {
   export type BodyType = TypedRequest<typeof roomSchemaValidate>;
   export const schema = yup.object().shape({ body: roomSchemaValidate });
+}
+
+export const roomSchemaId = yup.object({
+  id: yup.string().required()
+})
+
+export const roomSchemaStatusUpdate = yup.object({
+  status: yup.string().required()
+})
+
+export namespace UpdateStatusOfRoom {
+  export type ParamsType = TypedRequest<typeof roomSchemaId>
+  export type BodyType = TypedRequest<typeof roomSchemaStatusUpdate>
+  export const schema = yup.object().shape({
+    params: roomSchemaId,
+    body: roomSchemaStatusUpdate
+  })
 }
